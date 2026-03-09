53692cb1cbc141644e89f4c94adbcbcc88d2030e
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2026-03-09 11:24:24 -0500

    Add files via upload

---
+++ main_script2.5.R
@@ -0,0 +1,21 @@
+# main_template.R
+
+cat("Working directory:", getwd(), "\n")
+
+output_dir <- file.path(getwd(), "outputs/script2.5")
+dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+cat("Created directory:", output_dir, "\n")
+
+source("DTvsfDT.R")
+
+
+obs_gen_para <- read.csv("patter_new/obstacle_1.csv")
+
+dt <- 2.5 
+
+
+the_result <- fDT_Alg_save(obs_gen_para, dt)
+
+
+file_name <- file.path(output_dir, paste0("fDT2.5_1.rds"))
+saveRDS(the_result, file = file_name)
