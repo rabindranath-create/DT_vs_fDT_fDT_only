c7333c02968502fd4659c14cc26329ec607ae18d
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2026-03-26 22:22:51 -0500

    Add files via upload

--- DTvsfDT.R
+++ DTvsfDT.R
@@ -738,9 +738,6 @@ DT_vs_fDT_Alg <- function(obs_info, dt){
 
 
 
-
-
-
 #############
 #DT only for save
 ###########
@@ -819,9 +816,7 @@ DT_Alg_save <- function(obs_info){
     
     action_edge <- which.min(df_edge_ed[,4])
     
-    #explored_node
-    #explored_node <- c(explored_node, df_edge_ed$outside_vertex[action_edge])
-    explored_node <- c(df_edge_ed$outside_vertex[action_edge])
+    explored_node <- c(explored_node, df_edge_ed$outside_vertex[action_edge])
     
     ########
     #END: DT finding optimal action 
@@ -967,9 +962,7 @@ fDT_Alg_save <- function(obs_info, dt){
     
     action_edge <- which.min(df_edge_ed[,4])
     
-    #
-    #explored_node <- c(explored_node, df_edge_ed$outside_vertex[action_edge])
-    explored_node <- c(df_edge_ed$outside_vertex[action_edge])
+    explored_node <- c(explored_node, df_edge_ed$outside_vertex[action_edge])
     
     ########
     #END: DT finding optimal action 
